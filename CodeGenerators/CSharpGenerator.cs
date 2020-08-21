cddb2703af1bed066253f942e4337d8c115d5209
Cédric Luthi <cedric.luthi@gmail.com>
2020-08-21 08:34:55 +0200

    Use the built-in IndentedTextWriter class

--- CodeGenerators/CSharpGenerator.cs
+++ CodeGenerators/CSharpGenerator.cs
@@ -1,4 +1,6 @@
-﻿using Akzin.Crm.EarlyBoundGenerator.Helpers;
+﻿using System.CodeDom.Compiler;
+using System.IO;
+using Akzin.Crm.EarlyBoundGenerator.Helpers;
 using Akzin.Crm.EarlyBoundGenerator.ObjectModel;
 
 namespace Akzin.Crm.EarlyBoundGenerator.CodeGenerators
@@ -12,43 +14,43 @@ namespace Akzin.Crm.EarlyBoundGenerator.CodeGenerators
 
         }
 
-        public string Generate()
+        public void Generate(TextWriter textWriter)
         {
-            var writer = new Writer();
+            using var writer = new IndentedTextWriter(textWriter);
 
-            writer.AppendLine("using System;");
-            writer.AppendLine("using System.CodeDom.Compiler;");
-            writer.AppendLine("using System.ComponentModel;");
-            writer.AppendLine("using System.Diagnostics;");
-            writer.AppendLine("using System.Diagnostics.CodeAnalysis;");
-            writer.AppendLine("using System.Runtime.Serialization;");
-            writer.AppendLine("using Microsoft.Xrm.Sdk;");
-            writer.AppendLine("using Microsoft.Xrm.Sdk.Client;");
-            writer.AppendLine();
-            writer.AppendLine("// ReSharper disable All");
-            writer.AppendLine();
-            writer.AppendLine($"namespace {model.Namespace}");
-            writer.AppendLine("{");
-            writer.TabPush();
+            writer.WriteLine("using System;");
+            writer.WriteLine("using System.CodeDom.Compiler;");
+            writer.WriteLine("using System.ComponentModel;");
+            writer.WriteLine("using System.Diagnostics;");
+            writer.WriteLine("using System.Diagnostics.CodeAnalysis;");
+            writer.WriteLine("using System.Runtime.Serialization;");
+            writer.WriteLine("using Microsoft.Xrm.Sdk;");
+            writer.WriteLine("using Microsoft.Xrm.Sdk.Client;");
+            writer.WriteLineNoTabs("");
+            writer.WriteLine("// ReSharper disable All");
+            writer.WriteLineNoTabs("");
+            writer.WriteLine($"namespace {model.Namespace}");
+            writer.WriteLine("{");
+            writer.Indent++;
 
-            writer.AppendLine("[DataContract]");
-            writer.AppendLine("[ExcludeFromCodeCoverage]");
-            writer.AppendLine($@"[EntityLogicalName(""{model.LogicalName}"")]");
-            writer.AppendLine(@"[GeneratedCode(""Akzin.Crm.EarlyBoundGenerator"", ""1.0"")]");
-            writer.AppendLine($@"[DisplayName(""{model.DisplayName}"")]");
-            writer.AppendLine($@"[DebuggerDisplay(""{model.LogicalName} {{Id}} {{{model.Name?.LogicalName.ToPascalCase()}}}"")]");
-            writer.AppendLine($@"public partial class {model.LogicalName.ToPascalCase()} : Entity");
-            writer.AppendLine("{");
-            writer.TabPush();
+            writer.WriteLine("[DataContract]");
+            writer.WriteLine("[ExcludeFromCodeCoverage]");
+            writer.WriteLine($@"[EntityLogicalName(""{model.LogicalName}"")]");
+            writer.WriteLine(@"[GeneratedCode(""Akzin.Crm.EarlyBoundGenerator"", ""1.0"")]");
+            writer.WriteLine($@"[DisplayName(""{model.DisplayName}"")]");
+            writer.WriteLine($@"[DebuggerDisplay(""{model.LogicalName} {{Id}} {{{model.Name?.LogicalName.ToPascalCase()}}}"")]");
+            writer.WriteLine($@"public partial class {model.LogicalName.ToPascalCase()} : Entity");
+            writer.WriteLine("{");
+            writer.Indent++;
 
-            writer.AppendLine();
-            writer.AppendLine($@"public const string EntityLogicalName = ""{model.LogicalName}"";");
+            writer.WriteLineNoTabs("");
+            writer.WriteLine($@"public const string EntityLogicalName = ""{model.LogicalName}"";");
 
-            writer.AppendLine();
-            writer.AppendLine($@"public const string DisplayName = ""{model.DisplayName}"";");
+            writer.WriteLineNoTabs("");
+            writer.WriteLine($@"public const string DisplayName = ""{model.DisplayName}"";");
 
-            writer.AppendLine();
-            writer.AppendLine($@"public {model.LogicalName.ToPascalCase()}() :  base(EntityLogicalName) {{ }}");
+            writer.WriteLineNoTabs("");
+            writer.WriteLine($@"public {model.LogicalName.ToPascalCase()}() :  base(EntityLogicalName) {{ }}");
 
             WriteIdAttribute(writer);
             foreach (var attribute in model.Attributes)
@@ -56,133 +58,132 @@ namespace Akzin.Crm.EarlyBoundGenerator.CodeGenerators
                 WriteAttribute(writer, attribute);
             }
 
-            writer.TabPop();
-            writer.AppendLine("}");
+            writer.Indent--;
+            writer.WriteLine("}");
 
-            writer.TabPop();
-            writer.AppendLine("}");
-            return writer.ToString();
+            writer.Indent--;
+            writer.WriteLine("}");
         }
 
-        private static void WriteAttribute(Writer writer, Attribute attribute)
+        private static void WriteAttribute(IndentedTextWriter writer, Attribute attribute)
         {
-            writer.AppendLine();
-            writer.AppendLine($@"public const string {attribute.LogicalName.ToPascalCase()}Field = ""{attribute.LogicalName}"";");
+            writer.WriteLineNoTabs("");
+            writer.WriteLine($@"public const string {attribute.LogicalName.ToPascalCase()}Field = ""{attribute.LogicalName}"";");
 
-            writer.AppendLine();
-            writer.AppendLine($@"[AttributeLogicalName(""{attribute.LogicalName}"")]");
+            writer.WriteLineNoTabs("");
+            writer.WriteLine($@"[AttributeLogicalName(""{attribute.LogicalName}"")]");
 
             if (attribute.IsEnum)
             {
-                writer.AppendLine($@"public {attribute.TypeNameNullable} {attribute.LogicalName.ToPascalCase()}");
-                writer.AppendLine($@"{{");
-                writer.TabPush();
+                writer.WriteLine($@"public {attribute.TypeNameNullable} {attribute.LogicalName.ToPascalCase()}");
+                writer.WriteLine($@"{{");
+                writer.Indent++;
 
-                writer.AppendLine($@"get");
-                writer.AppendLine($@"{{");
-                writer.TabPush();
-                writer.AppendLine($@"var optionSet = GetAttributeValue<OptionSetValue>(""{attribute.LogicalName}"");");
-                writer.AppendLine($@"if (optionSet != null)");
-                writer.TabPush();
-                writer.AppendLine($@"return ({attribute.TypeName})Enum.ToObject(typeof({attribute.TypeName}), optionSet.Value);");
-                writer.TabPop();
-                writer.AppendLine($@"return null;");
-                writer.TabPop();
-                writer.AppendLine($@"}}");
+                writer.WriteLine($@"get");
+                writer.WriteLine($@"{{");
+                writer.Indent++;
+                writer.WriteLine($@"var optionSet = GetAttributeValue<OptionSetValue>(""{attribute.LogicalName}"");");
+                writer.WriteLine($@"if (optionSet != null)");
+                writer.Indent++;
+                writer.WriteLine($@"return ({attribute.TypeName})Enum.ToObject(typeof({attribute.TypeName}), optionSet.Value);");
+                writer.Indent--;
+                writer.WriteLine($@"return null;");
+                writer.Indent--;
+                writer.WriteLine($@"}}");
 
-                writer.AppendLine($@"set");
-                writer.AppendLine($@"{{");
-                writer.TabPush();
+                writer.WriteLine($@"set");
+                writer.WriteLine($@"{{");
+                writer.Indent++;
 
-                writer.AppendLine($@"if (value == null)");
-                writer.TabPush();
-                writer.AppendLine($@"SetAttributeValue(""{attribute.LogicalName}"", null);");
-                writer.TabPop();
-                writer.AppendLine($@"else");
-                writer.TabPush();
-                writer.AppendLine($@"SetAttributeValue(""{attribute.LogicalName}"", new OptionSetValue((int)value));");
-                writer.TabPop();
+                writer.WriteLine($@"if (value == null)");
+                writer.Indent++;
+                writer.WriteLine($@"SetAttributeValue(""{attribute.LogicalName}"", null);");
+                writer.Indent--;
+                writer.WriteLine($@"else");
+                writer.Indent++;
+                writer.WriteLine($@"SetAttributeValue(""{attribute.LogicalName}"", new OptionSetValue((int)value));");
+                writer.Indent--;
 
-                writer.TabPop();
-                writer.AppendLine($@"}}");
+                writer.Indent--;
+                writer.WriteLine($@"}}");
 
-                writer.TabPop();
-                writer.AppendLine($@"}}");
+                writer.Indent--;
+                writer.WriteLine($@"}}");
 
                 // enum
-                writer.AppendLine();
-                writer.AppendLine("[DataContract]");
-                writer.AppendLine(@"[GeneratedCode(""Akzin.Crm.EarlyBoundGenerator"", ""1.0"")]");
-                writer.AppendLine($@"public enum {attribute.LogicalName.ToPascalCase()}Enum");
-                writer.AppendLine("{");
-                writer.TabPush();
+                writer.WriteLineNoTabs("");
+                writer.WriteLine("[DataContract]");
+                writer.WriteLine(@"[GeneratedCode(""Akzin.Crm.EarlyBoundGenerator"", ""1.0"")]");
+                writer.WriteLine($@"public enum {attribute.LogicalName.ToPascalCase()}Enum");
+                writer.WriteLine("{");
+                writer.Indent++;
                 foreach (var option in attribute.Options)
                 {
-                    writer.AppendLine($@"[EnumMember] {option.Value.ToPascalCase()} = {option.Key},");
+                    writer.WriteLine($@"[EnumMember] {option.Value.ToPascalCase()} = {option.Key},");
                 }
-                writer.TabPop();
-                writer.AppendLine("}");
+                writer.Indent--;
+                writer.WriteLine("}");
             }
             else if (attribute.IsMoney)
             {
-                writer.AppendLine($@"public decimal? {attribute.LogicalName.ToPascalCase()}");
-                writer.AppendLine($@"{{");
-                writer.TabPush();
+                writer.WriteLine($@"public decimal? {attribute.LogicalName.ToPascalCase()}");
+                writer.WriteLine($@"{{");
+                writer.Indent++;
 
-                writer.AppendLine($@"get => GetAttributeValue<{attribute.TypeNameNullable}>(""{attribute.LogicalName}"")?.Value;");
-                writer.AppendLine($@"set => SetAttributeValue(""{attribute.LogicalName}"", value == null ? null : new Money(value.Value));");
+                writer.WriteLine($@"get => GetAttributeValue<{attribute.TypeNameNullable}>(""{attribute.LogicalName}"")?.Value;");
+                writer.WriteLine($@"set => SetAttributeValue(""{attribute.LogicalName}"", value == null ? null : new Money(value.Value));");
 
-                writer.TabPop();
-                writer.AppendLine($@"}}");
+                writer.Indent--;
+                writer.WriteLine($@"}}");
             }
             else
             {
-                writer.AppendLine($@"public {attribute.TypeNameNullable} {attribute.LogicalName.ToPascalCase()}");
-                writer.AppendLine($@"{{");
-                writer.TabPush();
+                writer.WriteLine($@"public {attribute.TypeNameNullable} {attribute.LogicalName.ToPascalCase()}");
+                writer.WriteLine($@"{{");
+                writer.Indent++;
 
-                writer.AppendLine($@"get => GetAttributeValue<{attribute.TypeNameNullable}>(""{attribute.LogicalName}"");");
-                writer.AppendLine($@"set => SetAttributeValue(""{attribute.LogicalName}"", value);");
+                writer.WriteLine($@"get => GetAttributeValue<{attribute.TypeNameNullable}>(""{attribute.LogicalName}"");");
+                writer.WriteLine($@"set => SetAttributeValue(""{attribute.LogicalName}"", value);");
 
-                writer.TabPop();
-                writer.AppendLine($@"}}");
+                writer.Indent--;
+                writer.WriteLine($@"}}");
             }
         }
 
-        private void WriteIdAttribute(Writer writer)
+        private void WriteIdAttribute(IndentedTextWriter writer)
         {
-            writer.AppendLine();
-            writer.AppendLine($@"public const string IdField = ""{model.Id.LogicalName}"";");
+            writer.WriteLineNoTabs("");
+            writer.WriteLine($@"public const string IdField = ""{model.Id.LogicalName}"";");
 
             // ID
-            writer.AppendLine();
-            writer.AppendLine($@"[AttributeLogicalName(""{model.Id.LogicalName}"")]");
-            writer.AppendLine($@"public override Guid Id");
-            writer.AppendLine($@"{{");
-            writer.TabPush();
-            writer.AppendLine($@"get => base.Id;");
-            writer.AppendLine($@"set => {model.Id.LogicalName.ToPascalCase()} = value;");
-            writer.TabPop();
-            writer.AppendLine($@"}}");
+            writer.WriteLineNoTabs("");
+            writer.WriteLine($@"[AttributeLogicalName(""{model.Id.LogicalName}"")]");
+            writer.WriteLine($@"public override Guid Id");
+            writer.WriteLine($@"{{");
+            writer.Indent++;
+            writer.WriteLine($@"get => base.Id;");
+            writer.WriteLine($@"set => {model.Id.LogicalName.ToPascalCase()} = value;");
+            writer.Indent--;
+            writer.WriteLine($@"}}");
 
-            writer.AppendLine();
-            writer.AppendLine($@"public const string {model.Id.LogicalName.ToPascalCase()}Field = ""{model.Id.LogicalName}"";");
+            writer.WriteLineNoTabs("");
+            writer.WriteLine($@"public const string {model.Id.LogicalName.ToPascalCase()}Field = ""{model.Id.LogicalName}"";");
 
-            writer.AppendLine();
-            writer.AppendLine($@"[AttributeLogicalName(""{model.Id.LogicalName}"")]");
-            writer.AppendLine($@"public Guid? {model.Id.LogicalName.ToPascalCase()}");
-            writer.AppendLine($@"{{");
-            writer.TabPush();
-            writer.AppendLine($@"get => GetAttributeValue<Guid?>(""{model.Id.LogicalName}"");");
-            writer.AppendLine($@"set");
-            writer.AppendLine($@"{{");
-            writer.TabPush();
-            writer.AppendLine($@"SetAttributeValue(""{model.Id.LogicalName}"", value);");
-            writer.AppendLine($@"base.Id = value ?? Guid.Empty;");
-            writer.TabPop();
-            writer.AppendLine($@"}}");
-            writer.TabPop();
-            writer.AppendLine($@"}}");
+            writer.WriteLineNoTabs("");
+            writer.WriteLine($@"[AttributeLogicalName(""{model.Id.LogicalName}"")]");
+            writer.WriteLine($@"public Guid? {model.Id.LogicalName.ToPascalCase()}");
+            writer.WriteLine($@"{{");
+            writer.Indent++;
+            writer.WriteLine($@"get => GetAttributeValue<Guid?>(""{model.Id.LogicalName}"");");
+            writer.WriteLine($@"set");
+            writer.WriteLine($@"{{");
+            writer.Indent++;
+            writer.WriteLine($@"SetAttributeValue(""{model.Id.LogicalName}"", value);");
+            writer.WriteLine($@"base.Id = value ?? Guid.Empty;");
+            writer.Indent--;
+            writer.WriteLine($@"}}");
+            writer.Indent--;
+            writer.WriteLine($@"}}");
         }
     }
 }
